47cc9e6aa6d7e56a3e1212da44e61b4b12922b91
nicolas balao <nicolasbalao.pro@gmail.com>
2022-12-01 15:48:18 +0100

    rice install.sh

--- install.sh
+++ install.sh
@@ -21,54 +21,57 @@
 # 2. tmux
 # 3. zsh
 #
-# ----------TEST---------------
-#   DOING- missing install for zsh-syntax and zsh-autosuggestion
+
+# Exit if any subcommand fails
+set -e
+
+#Global Var
+
+CLEAR_Line='\r\033[K'
 
 function requirements() {
-    echo "----Make update----"
-    sudo apt-get update
-    sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen make python3 python-is-python3
+
+    printf "${CLEAR_Line}[1/8] Updating system..."
+    sudo apt-get update -y > /dev/null
+
+    printf "${CLEAR_Line}[2/8] Install requirements..."
+    sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen make python3 python-is-python3 > /dev/null
 }
 
 function install_zsh() {
-    echo "----Installing ZSH----"
-    sudo apt-get install zsh
-
-    #make to default shell
-    chsh -s $(which zsh)
-    echo "Default shell: $SHELL"
+    printf "${CLEAR_Line}[3/8]  Install zsh..."
+    sudo apt-get install -y zsh > /dev/null
 }
 
 function install_oh_my_zsh() {
-    echo "----Install oh-my-zsh----"
+
+    printf "${CLEAR_Line}[4/8]  Install oh-my-zsh and plugins..."
     #https://ohmyz.sh/
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null
 }
 
 
 function install_autojump() {
-    echo "----Install autojump for oh-my-zsh----"
     cd /tmp
-    git clone https://github.com/wting/autojump
+    git clone https://github.com/wting/autojump > /dev/null
     cd autojump
-    ./install.py
+    ./install.py > /dev/null
 }
 
 function install_starship() {
-    echo "----Install starship----"
-    curl -sS https://starship.rs/install.sh | sh
+    printf "${CLEAR_Line}[5/8]  Install starship..."
+    curl -sS https://starship.rs/install.sh | sh 
 }
 
 function install_nvm() {
-    echo "----Install nvm----"
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+    printf "${CLEAR_Line}[5/8]  Install nvm..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.2/install.sh | bash
 }
 
 function install_shell() {
-    echo "----Setup the Terminal----"
     install_zsh
     install_oh_my_zsh
     install_autojump
@@ -77,34 +80,34 @@ function install_shell() {
 }
 
 function install_tmux() {
-    sudo apt-get install tmux
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    printf "${CLEAR_Line}[8/8]  Install tmux..."
+    sudo apt-get -y install tmux > /dev/null
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null
 }
 
 function install_neovim() {
+    printf "${CLEAR_Line}[7/8]  Install neovim from source..."
     cd /tmp
-    git clone https://github.com/neovim/neovim
+    git clone https://github.com/neovim/neovim > /dev/null
     cd /tmp/neovim/
-    make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make install
+    make CMAKE_BUILD_TYPE=RelWithDebInfo > /dev/null
+    sudo make install  > /dev/null
 }
 
 function install_tools() {
-    echo "----Install tmux----"
     install_tmux
-    echo "----Install neovim----"
     install_neovim
 }
 
 function link_dotfile() {
-    echo "Export config path $1"
-    mv ~/.config/nvim ~/.config/nvim.bak
-    mv ~/.tmux.conf ~/.tmux.bak
-    mv ~/.zshrc ~/.zshrc.bak
+    printf "${CLEAR_Line}[8/8]  Install config files..."
+    mv ~/.config/nvim ~/.config/nvim.bak 2> /dev/null
+    mv ~/.tmux.conf ~/.tmux.bak 2> /dev/null
+    mv ~/.zshrc ~/.zshrc.bak 2> /dev/null
 
-    ln -s $1/tmux/tmux.conf ~/.tmux.conf
-    ln -s $1/nvim ~/.config/nvim
-    ln -s $1/zsh/zshrc ~/.zshrc
+    ln -s $1/tmux/tmux.conf ~/.tmux.conf 2> /dev/null
+    ln -s $1/nvim ~/.config/nvim 2> /dev/null
+    ln -s $1/zsh/zshrc ~/.zshrc 2> /dev/null
 }
 
 function main() {
@@ -114,6 +117,11 @@ function main() {
     install_shell
     install_tools
     link_dotfile $path
+
+    echo "Dont forget to install nerd font: https://nerdfonts.com"
+    echo "Setting up"
+    echo "neovim: :PackerInstall "
+    echo "tmux: C-a I"
 }
 
 main
