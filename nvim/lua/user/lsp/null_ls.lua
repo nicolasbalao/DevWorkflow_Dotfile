a8468aa57a811afa8e0178fcac43e91c8cc04af7
nicolas balao <nicolasbalao.pro@gmail.com>
2022-12-02 17:50:41 +0100

    format(nvim): remove code comment

--- nvim/lua/user/lsp/null_ls.lua
+++ nvim/lua/user/lsp/null_ls.lua
@@ -14,38 +14,3 @@ null_ls.setup({
 	},
 })
 
---[[
--- Testing setup automatically with the mason api
-
-local mason_registry_ok, mason_registry = pcall(require, "mason-registry")
-
-if not mason_registry_ok then
-	return
-end
-
-local show = function(table)
-	print(vim.inspect(table))
-end
-
-
-local my_package_installed = mason_registry.get_installed_packages()
-local formatter = {}
-local linter = {}
-local dap = {}
-
-for _, package in pairs(my_package_installed) do
-	--print(package.name)
-	--show(package.spec.categories[1])
-	local categories = package.spec.categories[1]
-
-	if categories == "Formatter" then
-		table.insert(formatter, package.name)
-	elseif categories == "Linter" then
-		table.insert(linter, package.name)
-    elseif categories == "DAP" then
-        table.insert(dap, package.name)
-	end
-end
-
-show(formatting.stylua)
-]]
