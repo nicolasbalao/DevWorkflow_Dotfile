177639e5444ebf9a710176c82658f6ee9542e3c7
nicolas balao <nicolasbalao.pro@gmail.com>
2022-12-04 15:02:42 +0100

    refactor(nvim): refactor mason config with error handling

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,4 +1,23 @@
-require("mason").setup({
+local mason_ok, mason = pcall(require, "mason")
+local mason_lsp_ok, mason_lsp = pcall(require, "mason-lspconfig")
+local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
+
+if not mason_ok then
+	print("Missing Mason")
+	return
+end
+
+if not mason_lsp_ok then
+	print("Missing Mason_lsp")
+	return
+end
+
+if not lspconfig_ok then
+	print("Missing lspconfig")
+	return
+end
+
+mason.setup({
 	ui = {
 		icons = {
 			package_installed = "✓",
@@ -8,18 +27,17 @@ require("mason").setup({
 	},
 })
 
-require("mason-lspconfig").setup({
+mason_lsp.setup({
 	ensure_installed = { "sumneko_lua", "tsserver", "clangd" },
 })
 
-require("mason-lspconfig").setup_handlers({
+mason_lsp.setup_handlers({
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
 	function(server_name) -- default handler (optional)
-		require("lspconfig")[server_name].setup({
+		lspconfig[server_name].setup({
 			on_attach = require("user.lsp.handler").on_attach,
 		})
 	end,
 })
-
