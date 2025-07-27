e2b2a2aa66d03db50fa0ae206b05dabcd895d3a3
Nicolas Balao <nicolasbalao.pro@gmail.com>
2025-07-27 16:22:19 +0200

    feat(nvim): Keymaps tmux-sessionizer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,11 +8,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- vim.keymap.set("n", "<leader>f", function()
---   require("conform").format({ bufnr = 0 })
--- end)
+vim.keymap.set("n", "<C-f>","<cmd>silent !tmux neww tmux-sessionizer<CR>");
 
 -- Format avec LSP ou Conform
 vim.keymap.set("n", "<leader>f", function()
   require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format buffer" })
+
+
