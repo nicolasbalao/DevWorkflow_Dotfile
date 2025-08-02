6be94e4998cbf75075026465ae2874d27a076e95
Nicolas Balao <nicolasbalao.pro@gmail.com>
2025-08-02 17:13:10 +0200

    feat(fish): Add Git abbrs

---
+++ fish/conf.d/git-abbrs.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    abbr -a g   git
+    abbr -a ga  git add
+    abbr -a gc  git commit
+    abbr -a gcm git commit -m
+    abbr -a gs  git status
+    abbr -a gl  git log --oneline --graph --decorate
+    abbr -a gp  git push
+    abbr -a gpo git push origin
+    abbr -a gpl git pull
+    abbr -a gco git checkout
+    abbr -a gcb git checkout -b
+    abbr -a gb  git branch
+    abbr -a gd  git diff
+    abbr -a gds git diff --staged
+    abbr -a gr  git restore
+    abbr -a grs git restore --staged
+    abbr -a gcl git clone
+    abbr -a gsw git switch
+    abbr -a gsb git switch -c
+end
