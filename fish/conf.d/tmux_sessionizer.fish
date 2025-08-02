0d6ca4400e44d2eaf1016a16a16ce96d7c0749f9
Nicolas Balao <nicolasbalao.pro@gmail.com>
2025-08-02 17:08:01 +0200

    feat(fish): Add function for going back to the root dir when you are in tmux

--- fish/conf.d/tmux_sessionizer.fish
+++ fish/conf.d/tmux_sessionizer.fish
@@ -4,4 +4,14 @@ if status is-interactive && ensure tmux && ensure tmux-sessionizer
 
     bind -M default \cf 'tmux-sessionizer'
     bind -M default \ch 'tmux neww tmux-sessionizer -l'
+
+    if test -n $TMUX
+        # cdp allow to quickly return back to the project
+        function cdp
+            set -l project (tmux showenv ROOT_DIR | string replace 'ROOT_DIR=' '')
+            cd $project
+        end
+    end
 end
+
+
